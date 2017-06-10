68fde6efb2ed5219fe20d7f105235bd633930d70
David Rubin <dudrubin@gmail.com>
2017-06-10 20:21:01 +0300

    Added items& opening page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,31 @@
  * Controller of the mockApp
  */
 angular.module('mockApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope) {
+    $scope.data = [
+      {
+        name:"מחשבים",
+        products: 4,
+        saleIndex: 2,
+        mainImage:"https://pisces.bbystatic.com/BestBuy_US/store/ee/2015/com/pm/nav_desktops_1115.jpg;maxHeight=288;maxWidth=520",
+        itemImage:"http://images.computerhistory.org/timeline/timeline_computers_1983.lisa.jpg"
+      },
+      {
+        name:"טלפונים",
+        products: 10,
+        saleIndex: 6,
+        mainImage:"http://www.ywcancin.org/atf/cf/%7B6C13D2EE-5ADB-4290-8164-67CDE6DEDE7D%7D/Cell%20Phones%20&%20Smartphones.jpg",
+        itemImage:"https://usercontent2.hubstatic.com/1397827.jpg"
+      },
+      {
+        name:"קשקושים",
+        products: 8,
+        saleIndex: 4,
+        mainImage:"http://www.explorecarrental.com/images/party.jpg",
+        itemImage:"http://hireking.co.nz/image/data/Party%20Supplies.jpg"
+      },
     ];
+    $scope.getNumber = function(num) {
+      return new Array(num);
+    };
   });
